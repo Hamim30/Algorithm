848b15650a9e564fed09c01c762ba48be2e19792
Hamim z <67522425+Hamim30@users.noreply.github.com>
2024-01-26 11:58:48 +0600

    Update multiscale_retinex.py

--- multiscale_retinex.py
+++ multiscale_retinex.py
@@ -5,4 +5,5 @@ def msr():
         image = read_show("dt2",i,show=False)
         msrcr_img = MSRCR(image,config.SIGMA_LIST,config.ALPHA,config.BETA,config.G,config.OFFSET)
         cv2.imwrite(f'dt2_msr/{i}',msrcr_img)
+
 msr()
